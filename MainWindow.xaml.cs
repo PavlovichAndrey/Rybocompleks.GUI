b96773fe8712c847e2ea2bedef799b38887924b4
Pavlovich Andrei <mira69.27@gmail.com>
2016-11-10 22:31:29 +0300

    Добавлена StartPage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System. Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,7 +17,6 @@ using System.Xml;
 
 namespace Rybocompleks.GUI
 {
-
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -33,24 +32,20 @@ namespace Rybocompleks.GUI
                 this.Name = Name;
                 this.Value = Value;
             }
-            public DataSensors()
-            {
-                this.Name = null;
-                this.Value = 0;
-            }
         }
         List<DataSensors> sensors = new List<DataSensors>();
         public MainWindow()
         {
             InitializeComponent();
-            XmlDocument doc = new XmlDocument();
+            /*XmlDocument doc = new XmlDocument();
             doc.Load("Sensors.xml");
             foreach (XmlNode node in doc.DocumentElement)
             {
                 string name = node.Attributes[0].Value;
                 int value = Int32.Parse(node.Attributes[1].Value);
-                sensors.Add(new DataSensors(name,value));
-            }
+                sensors.Add(new DataSensors(name, value));
+            }*/
+        
         }
 
         private void FileExit_Click(object sender, RoutedEventArgs e)
@@ -60,21 +55,53 @@ namespace Rybocompleks.GUI
 
         private void SensorTable_Loaded(object sender, RoutedEventArgs e)
         {
-            SensorTable.ItemsSource = sensors;
+            //SensorTable.ItemsSource = sensors;
         }      
 
-        private void MenuOpenGP_Click(object sender, RoutedEventArgs e)
+        private void OpenGP_Click(object sender, RoutedEventArgs e)
         {
-
+            BrowseBtn_Click(sender,e);
         }
-
-        private void MenuRun_Click(object sender, RoutedEventArgs e)
+         
+        private void Run_Click(object sender, RoutedEventArgs e)
         {
+            if(TxtBx_GPPath.Text!="")
+            {
+                if(System.IO.File.Exists(TxtBx_GPPath.Text))
+                    MessageBox.Show("Запуск процесса выращивания рыбы по плану из файла:\n "+TxtBx_GPPath.Text);
+                else
+                    MessageBox.Show("Файл не найден", "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MessageBox.Show("Имя файла не может быть пустым!", "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         private void MenuAbout_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Вывод информации о проекте");
         }
+
+        private void BrowseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = ""; // Default file name
+            dlg.DefaultExt = ".xml"; // Default file extension
+            dlg.Filter = "xml documents (.xml)|*.xml"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                TxtBx_GPPath.Text = filename;
+            }
+        }
+
+      
     }
 }
